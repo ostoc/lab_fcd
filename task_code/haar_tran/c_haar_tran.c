b66823d12adc92fd760886c97d532f135d729fac
Junyu Pu <pujunyu@gmail.com>
2014-07-13 17:24:23 +0200

    Update the haar_tran, and readme.txt
    change the the haar_tran to parallel, and readme.txt to read me.md

--- task_code/haar_tran/c_haar_tran.c
+++ task_code/haar_tran/c_haar_tran.c
@@ -10,6 +10,8 @@
 #include <string.h>
 #include <sys/ipc.h>
 #include <semaphore.h>
+#include <sys/time.h>
+
 
 #define pixel(x,y) pixels[((y)*size)+(x)]
 #define MAX_MSG_SIZE 1024
@@ -18,9 +20,9 @@
 #define MSG_TYPE_FINISH_THREAD 2
 
 
-int max_threads = 7; // change the thread number here
+int max_threads; // change the thread number here
 
-int *pixels;
+int *pixels=0;
 
 void *thread_function (void *ptr);
 
@@ -49,7 +51,21 @@ int no_of_completions = 0;
 
 long long int size;
 
+double time_diff(struct timeval x , struct timeval y)
+{
+    double x_ms , y_ms , diff;
+     
+    x_ms = (double)x.tv_sec*1000000 + (double)x.tv_usec;
+    y_ms = (double)y.tv_sec*1000000 + (double)y.tv_usec;
+     
+    diff = (double)y_ms - (double)x_ms;
+     
+    return diff;
+}
+
 int main(int argc, char *argv[]){
+      printf("Please enter the number of cores you want to use: ");
+      scanf("%d", &max_threads);      
       FILE *in;
       FILE *out;
 			int i, iret1;
@@ -70,21 +86,23 @@ int main(int argc, char *argv[]){
       int x, y;
       long long int a, d;
       double SQRT_2;
-      fread(&size, sizeof(size), 1, in);  
-      fwrite(&size, sizeof(size), 1, out);     
+      int ret_r = fread(&size, sizeof(size), 1, in);  
+      int ret_w = fwrite(&size, sizeof(size), 1, out);     
       pixels = (int *) malloc(size * size * sizeof(int)); 
+			//int pixels[size*size];
+
       if (!fread(pixels, size * size * sizeof(int), 1, in)) { 
       perror("read error");
       exit(EXIT_FAILURE);
       }
 			// fork thread 
-			
+
 			if ( sem_init (&mutex_finish_task,0,0) < 0 )
 			{
 				perror("sem_init");
 				exit(1);
 			}
-			
+
 			if ( sem_init (&mutex_shared_data,0,1) < 0 )
 			{
 				perror("sem_init");
@@ -103,8 +121,8 @@ int main(int argc, char *argv[]){
                 perror("msgget");
                 exit(1);
         }
-			
-			
+
+
 			//Allocate memory for list of tasks;
 			list_of_tasks = (struct msgbuf_send_task *)malloc(sizeof(struct msgbuf_send_task) );
 			if(list_of_tasks ==0)
@@ -129,6 +147,10 @@ int main(int argc, char *argv[]){
 			}
 
       SQRT_2 = sqrt(4);
+			struct timeval before , after;
+	    gettimeofday(&before , NULL);
+
+
       for (s = size; s > 1; s /= 2) {
  
       mid = s / 2;
@@ -145,7 +167,7 @@ int main(int argc, char *argv[]){
                 exit(1);
         }
 
-		
+			/*
       for (x = 0; x < mid; x++) {
                    a = pixel(x,y);
                    a = (a+pixel(mid+x,y))/SQRT_2;
@@ -154,9 +176,9 @@ int main(int argc, char *argv[]){
                    pixel(x,y) = a;
                    pixel(mid+x,y) = d;
             } 
-    
+    	*/
 			}
-			
+
 			//Now we wait for all processes to finish their task
 			for(y=0; y<mid; y++)
 			{
@@ -169,9 +191,9 @@ int main(int argc, char *argv[]){
       
       // column-transformation
 			for (x = 0; x < mid; x++) {
-			
+
 				list_of_tasks->mtype = MSG_TYPE_SEND_TASK;
-					
+
 			sprintf (list_of_tasks->mtext, "%d %d %d %d %d", x, x, 0, (int)mid, (int)mid); //x_min, x_max, y_min, y_max, mid
 			//send a message to a task
 			if((msgsnd(msgqueue_id_send_task, (struct msgbuf_send_task *)(list_of_tasks),
@@ -182,7 +204,7 @@ int main(int argc, char *argv[]){
         }
 
 
-		
+			/*
       for (y = 0; y < mid; y++) {
       
                    a = pixel(x,y);
@@ -192,7 +214,8 @@ int main(int argc, char *argv[]){
                    pixel(x,y) = a;
                    pixel(x,mid+y) = d;
             }
-			    
+			*/
+
 			}
 			//Now we wait for processes to finish the task
 			for(x=0; x<mid; x++)
@@ -205,19 +228,21 @@ int main(int argc, char *argv[]){
 			}
 
       }
-			
+			gettimeofday(&after , NULL);
+ 
+	    printf("\nTotal time elapsed : %.0lf us\n" , time_diff(before , after) );
 
       fwrite(pixels, size * size * sizeof(int), 1, out);
       free(pixels); 
       fclose(out); 
       fclose(in);
-			
+
 			printf("Thread %d, working...", max_threads);
 
 			for (i=0; i <max_threads ; i++)
 			{
 				list_of_tasks->mtype = MSG_TYPE_FINISH_THREAD;
-	
+
 
 
 			if((msgsnd(msgqueue_id_send_task, (struct msgbuf_send_task *)(list_of_tasks),
@@ -228,8 +253,8 @@ int main(int argc, char *argv[]){
         }
 
 			}
-			
-	
+
+
 			for(i=0;i<max_threads; i ++)
 			{
 				pthread_join( thread_id[i], NULL);
@@ -238,7 +263,7 @@ int main(int argc, char *argv[]){
       
 printf("\nFinished\n");
 return EXIT_SUCCESS;
-	
+
       
 }
 
@@ -253,6 +278,14 @@ void *thread_function(void * ptr)
 	int x,y, a,d;
   int SQRT_2 = 2;
   int index_temp_array = 0;
+	if(len_temp_array < (size))
+			  {
+					temp_array  = (int *)realloc(temp_array, (size) );
+					len_temp_array = size;
+					
+				}
+	memset(temp_array, 0, (sizeof(int)) * len_temp_array); 
+
 	while(1)
 	{	
 		if (msgrcv(msgqueue_id_send_task, &rx_msg, MAX_MSG_SIZE, 0, 0) < 0) {
@@ -271,31 +304,40 @@ void *thread_function(void * ptr)
 		sscanf	(rx_msg.mtext, "%d %d %d %d %d", 	&x_min, &x_max, &y_min, &y_max, &mid);
 
     //put a sanity check to make sure they are all >0
-		if(len_temp_array < (x_max - x_min + y_max -y_min))
-			  {
-					temp_array  = (int *)realloc(temp_array, (x_max - x_min + y_max -y_min) );
-					len_temp_array = (x_max - x_min + y_max -y_min);
-				}
-		
-		//Lock the shared data structure and make a copy
+
+		//Lock the shared data structure and make a copy	
+		/*
 		if(sem_wait(&mutex_shared_data) <0){
 			perror("sem_wait:");
 			exit(0);
 		}
+		*/
+
 		//make a copy of data struct
+		/*
 		index_temp_array = 0;
-		for(x=x_min; x<x_max ; x++)
-			for (y=y_min; y<y_max; y++)
+		
+		if(x_min==x_max)
+			for (y=y_min; y<size; y++)
 				{
-					temp_array[index_temp_array] = pixel(x,y);
+					temp_array[index_temp_array] = pixel(x_min,y);
 					index_temp_array++;
-				}								
+				}						
+		if(y_min==y_max)
+			for (x=x_min; x<size; x++)
+				{
+					temp_array[index_temp_array] = pixel(x,y_min);
+					index_temp_array++;
+				}		
+		*/
 		//unlock the data structure		
+		/*
 		if(sem_post(&mutex_shared_data) <0){
 			perror("sem_post:");
 			exit(0);
 		}
-		
+		*/
+		/*
     for (x = 0; x < mid; x++) {
           a = temp_array[x];
           a = (a+temp_array[mid+x])/SQRT_2;
@@ -304,28 +346,62 @@ void *thread_function(void * ptr)
           temp_array[x] = a;
           temp_array[mid+x] = d;
             } 
-	 //Lock the shared data structure and make a copy
+		*/
+		if(y_min == y_max){
+					y = y_min;
+	       for (x = 0; x < mid; x++) {
+                   a = pixel(x,y);
+                   a = (a+pixel(mid+x,y))/SQRT_2;
+                   d = pixel(x,y);
+                   d = (d-pixel(mid+x,y))/SQRT_2;
+                   pixel(x,y) = a;
+                   pixel(mid+x,y) = d;
+            }
+			}
+		if(x_min == x_max){
+				x=x_min;
+				for (y = 0; y < mid; y++) {
+      
+                   a = pixel(x,y);
+                   a = (a+pixel(x,mid+y))/SQRT_2;
+                   d = pixel(x,y);
+                   d = (d-pixel(x,mid+y))/SQRT_2;
+                   pixel(x,y) = a;
+                   pixel(x,mid+y) = d;
+            }	 
+			}
+		//Lock the shared data structure and make a copy
+		/*		
 		if(sem_wait(&mutex_shared_data) <0){
 			perror("sem_wait:");
 			exit(0);
 		}
-		
+		*/
 		//copy the data structures back to main..
-	  index_temp_array = 0;
-		for(x=x_min; x<x_max ; x++)
-			for (y=y_min; y<y_max; y++)
+		/*	  
+		index_temp_array = 0;
+		if(x_min==x_max)
+			for (y=y_min; y<size; y++)
 				{
-					pixel(x,y) = temp_array[index_temp_array] ;
+					pixel(x_min,y) = temp_array[index_temp_array] ;
 					index_temp_array++;
-				}	
+				}						
+		if(y_min==y_max)
+			for (x=x_min; x<size; x++)
+				{
+					pixel(x,y_min) = temp_array[index_temp_array] ;
+					index_temp_array++;
+				}		
 
-
+		*/
 		//unlock the data structure		
+		/*		
 		if(sem_post(&mutex_shared_data) <0){
 			perror("sem_post:");
 			exit(0);
 		}
-		
+		*/
+
 		if(sem_post(&mutex_finish_task) <0)
 				{
 					perror("sem_wait");
@@ -336,11 +412,10 @@ void *thread_function(void * ptr)
 
 	case 	MSG_TYPE_FINISH_THREAD:
 		return NULL;
-	
+
    default:
 		printf("\n Unrecognized message %d, MsgData = %s", (int)rx_msg.mtype, rx_msg.mtext);
 		return NULL;
 	}
 }
 }
-
